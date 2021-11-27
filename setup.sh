40ffe07d9a9895b5dd7553a1a1ec162041e5f454
Niko Laaksonen <niko.laaksonen@solita.fi>
2021-11-27 18:58:08 +0200

    Updates setup.sh to allow for arbitrary remote folders from our Solitabay.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt-get install libsox-fmt-all libsox-dev sox > /dev/null
 mkdir -p /content/data/converted /content/data/raw /content/modules
 wget -q -O /content/data.zip https://solitabay.solita.fi/index.php/s/ZEbs8DcwbQqCWJA/download
-unzip -j /content/data.zip "machine_spraak/raw_audio_data_20211007/*" -d /content/data/raw/
+unzip -j /content/data.zip "machine_spraak/$1/*" -d /content/data/raw/
 rm /content/data.zip
 for file in /content/data/raw/*.WAV
 do
